33ec0a085ed0e6835f563df0f0e94fc6f58aff11
Ali <bmsmmrdi1997@gmail.com>
2023-07-27 22:53:52 +0500

    my third file git tack

---
+++ script.js
@@ -0,0 +1,25 @@
+let num =50
+
+// example with while 
+// while(num <55){
+//     console.log(num);
+//     num++;
+// }
+
+// example with do while
+
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while(num<55);
+
+// example with for
+
+for(i=0;i<15;i++){
+if(i=== 5){
+    continue;
+}
+console.log(i);
+   
+}
